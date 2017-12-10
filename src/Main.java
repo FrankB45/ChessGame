149539dd036e60120242511566a7cb7c0cb2e25f
frankb45 <frankst3r@Optonline.net>
2017-12-09 22:59:23 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,56 @@
-public class Main
+import javax.swing.*;
+
+public class Main implements Runnable
 {
-    public static void main(String args[])
+    private Model model;
+    private JFrame mainFrame;
+    public Main(Model m)
     {
+        //Applies model to global variable
+        model =m;
+    }
+    @Override
+    public void run()
+    {
+        //Creates Main JFrame
+        mainFrame = new JFrame("Chess Game v0.1a");
+        //Main Content Panel
+        JPanel mainPanel = new JPanel();
+        //Links Content Panel to the JFrame
+        mainFrame.getContentPane().add(mainPanel);
+        /**///////////
+
+        //Loads GUI elements
+        new GUI(model,mainPanel,this);
+        //Makes Frame
+        frameBasics(mainFrame);
+
 
     }
+    public void updateJFrame()
+    {
+        frameUpdate(mainFrame);
+    }
+    public void frameUpdate(JFrame f)
+    {
+        f.pack();
+        f.repaint();
+    }
+    public void frameBasics(JFrame f)
+    {
+        f.setVisible(true);
+        f.pack();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String args[])
+    {
+        //Creates Model
+        Model m = new Model();
+        SwingUtilities.invokeLater(new Main(m));
+    }
+
 
 
 }
--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Model
 {
+    private Piece[][] board = new Piece[8][8];
+    public Model()
+    {
 
+    }
 
 }
